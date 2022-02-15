09244b5f9981fd16cf9a98a5e53e1b9a667d26c8
Philani Ncube <ncbphi001@gmail.com>
2022-02-16 00:10:43 +0200

    hover text

---
+++ components/Vision.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Vision = () => {
+  return (
+    <div className="hidden lg:block bg-yellow-400 p-4 w-[350px] md:h-[50%] text-blue-600 absolute top-[28%]">
+      <h3 className="font-bold text-lg">Our Vision & Mission</h3>
+      <p className="text-sm">
+        Promoting a healthy lifestyle amongst the local community at affordable
+        rates.
+      </p>
+      <h3 className="font-bold text-lg">Goals</h3>
+      <ul className="text-sm list-disc pl-4">
+        <li>To get as many people interested in exercing</li>
+        <li>To encourage and promote an active society</li>
+        <li>
+          To empower local communities by creating employment opportunities for
+          the youth and reducing gangster activities
+        </li>
+      </ul>
+      <h3 className="font-bold text-lg">Our Values</h3>
+      <p className="text-sm">
+        Fitness HQ is about an active society, health and givig back to the
+        society by assisting youth to move away from gangester activities.
+      </p>
+    </div>
+  );
+};
+
+export default Vision;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaWhatsapp,
-  FaTwitter,
-} from 'react-icons/fa';
-import Social from '../components/Social';
+import { useState } from 'react';
 
-const links = [
-  { icon: <FaFacebookF />, href: 'https://www.facebook.com' },
-  { icon: <FaInstagram />, href: 'https://www.instagram.com/abrahams6237/' },
-  { icon: <FaWhatsapp />, href: 'https://www.whatsapp.com' },
-  { icon: <FaTwitter />, href: 'https://twitter.com' },
-];
+import Social from '../components/Social';
+import Vision from '../components/Vision';
 
 export default function Home() {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="bg-blue-800 h-screen relative">
       <div className="hidden md:block md:absolute min-w-1/2 top-0 right-0 h-[100vh] bg-white pointer-events-none">
+        {show && <Vision />}
         <img
           src="/images/rack.webp"
           alt=""
@@ -26,9 +19,11 @@ export default function Home() {
         />
       </div>
       <div
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
         role="decoration"
         aria-hidden
-        className="hidden absolute top-[28%] md:block md:right-[44.5%] md:w-[15px] bg-yellow-400 md:h-[50%] pointer-events-none"
+        className="hidden absolute top-[28%] md:block md:right-[44.5%] md:w-[15px] bg-yellow-400 md:h-[50%] cursor-pointer"
       ></div>
       <div className="max-w-6xl mx-auto z-40">
         <div className="flex flex-col h-[100vh] w-full sm:w-2/3 md:w-1/2 px-4 md:pr-10 justify-center space-y-8">
