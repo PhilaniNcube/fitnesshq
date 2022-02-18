4403f1b0149211866c3a45c052225b910e46a3b6
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2022-02-18 11:15:37 +0200

    Update FAQ.jsx

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -5,15 +5,11 @@ const FAQ = () => {
     <div className="hidden lg:block bg-yellow-400 p-4 w-[350px] text-blue-600 absolute -top-[250%] -right-[300%] z-80">
       <h3 className="font-bold text-lg">How do I join FitnessHQ?</h3>
       <p className="text-sm italic">
-        You can visit the club or register online get a membership
+        You can send us a message and we can sign you up.
       </p>
       <h3 className="font-bold text-lg">Do I have to be fit to join?</h3>
       <p className="text-sm italic">
-        Anyone can join you do not need to be fit we start training at your
-        fitness or endurance levels. We work with equipment but start one week
-        using our bodyweight and we adapt and progress from there depending on
-        your goals we have a progress report we are we write everything down and
-        revisit our goals every month
+ Anyone can join you do not need to be fit. We start training at your fitness or endurance levels. We work with equipment but start one week using bodyweight and we adapt depending on your goals.
       </p>
 
       <h3 className="font-bold text-lg">Is the club in a safe environment?</h3>
@@ -23,8 +19,9 @@ const FAQ = () => {
       </h3>
       <p className="text-sm italic">
         You will be training with a qualified professional personal trainer with
-        great experience , registered with REPSSA.
+        great experience , registered with Register OF Excercise Professionals South Africa.
       </p>
+    <h3 className="font-bold text-lg">we do not want to be the best but bring out the best in others.</h3>
     </div>
   );
 };
